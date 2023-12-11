829773c93b2e489cafa527de4407467706968849
JeyhunKarimkhanov <ckerimxanov50@gmail.com>
2023-12-11 20:41:03 +0100

    Module 4 Assignment

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+let names = ["Jeyhun", "Haakov", "John", "Jacob", "Marcel", "Paula"]
+
+for (let i = 1; i < names.length; i+=2) {
+    console.log("Hello" + " " + names[i])
+}
+
+for(let i = 0; i<names.length; i+=1){
+    console.log("Good Morning" + " " + names[i])
+}
